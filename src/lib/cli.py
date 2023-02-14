cd50fc6eaf41aafb164a7eed8aebd913364979bb
Thanatisia <55834101+Thanatisia@users.noreply.github.com>
2023-02-14 23:58:35 +0800

    Update 2023-12-14 2357H | v0.1.1 - Added 'list_vms' function to src/lib/vbox.py - Added 'get_value' function to src/lib/cli.py - Testing

--- src/lib/cli.py
+++ src/lib/cli.py
@@ -15,10 +15,18 @@ class CLI():
         self.class_name = "cli"
         self.description = "This class/library handles CLI and shell command/scripting support in python using subprocess"
 
-    def new_pipe(self, command, **options):
+    def new_pipe(self, cmd, **options):
         """
         Open a new process pipe and 
         execute a new command
+
+        :: Params
+        :: - Positionals
+        ::  + cmd : List of command and parameters to execute
+        ::      - Syntax: [command, argument-1, argumnet-2, ...]
+        ::      - Type: List
+        ::  + options : Subprocess options kwargs
+        ::      - Type: kwargs (Dictionary)
         """
         process = sp.Popen(cmd, **options)
         return process
@@ -26,6 +34,10 @@ class CLI():
     def is_alive(self, process):
         """
         Check if process is still alive
+
+        :: Params
+        :: - process : The process you wish to extract values from
+        ::  Type: subprocess.Popen()
         
         None: Alive, Not Terminated
         Else: Not Alive; Terminated
@@ -40,6 +52,23 @@ class CLI():
 
         # Return result
         return ret_Value
+
+    def get_value(self, process): 
+        """
+        Get standard output from a process pipe
+
+        :: Params
+        :: - process : The process you wish to extract values from
+        ::  Type: subprocess.Popen()
+        """
+        # Initialize Variables
+        values = []
+        alive_Check = self.is_alive(process)
+        if(alive_Check == "Alive"):
+            # If process is alive
+            # Get standard output from process
+            values.append(process.communicate())
+        return values
 
 class UnitTests():
     def test_import(self):
--- src/lib/vbox.py
+++ src/lib/vbox.py
@@ -8,9 +8,22 @@ from subprocess import Popen, PIPE
 ### External Libraries
 import virtualbox as vbox
 
+### Personal Libraries
+import lib.cli as cli
+
 class VirtualBox():
     def __init__(self):
         self.class_name = "VirtualBox"
+        self.clihelper = cli.CLI()
+
+    def list_vms(self):
+        """
+        List all running Virtual Machines
+        """
+        proc = self.clihelper.new_pipe(["vboxmanage", "list", "vms"], stdin=PIPE, stdout=PIPE)
+        # Get values
+        all_vms = self.clihelper.get_value(proc)
+        print("Virtual Machines : {}".format(all_vms))
 
 class UnitTests():
     def test_import(self):
